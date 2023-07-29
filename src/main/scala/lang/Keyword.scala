a0cfda789bd35ddb456f9382ac949e9b3eed47e8
Valentin Aebi <valentin.aebi@epfl.ch>
2023-07-29 10:24:19 +0200

    Sort keywords enum

--- src/main/scala/lang/Keyword.scala
+++ src/main/scala/lang/Keyword.scala
@@ -1,22 +1,22 @@
 package lang
 
 enum Keyword(val str: String) {
-  case Val extends Keyword("val")
-  case Var extends Keyword("var")
-  case If extends Keyword("if")
+  case Arr extends Keyword("arr")
+  case As extends Keyword("as")
   case Else extends Keyword("else")
-  case When extends Keyword("when")
-  case Then extends Keyword("then")
-  case While extends Keyword("while")
-  case For extends Keyword("for")
   case Fn extends Keyword("fn")
+  case For extends Keyword("for")
+  case If extends Keyword("if")
+  case New extends Keyword("new")
+  case Panic extends Keyword("panic")
   case Return extends Keyword("return")
   case Struct extends Keyword("struct")
-  case Arr extends Keyword("arr")
-  case New extends Keyword("new")
-  case As extends Keyword("as")
-  case Panic extends Keyword("panic")
   case Test extends Keyword("test")
+  case Then extends Keyword("then")
+  case Val extends Keyword("val")
+  case Var extends Keyword("var")
+  case When extends Keyword("when")
+  case While extends Keyword("while")
 
   override def toString: String = str
 }
