d2e1cec471109e841425b389f2a17b15921fb89b
Valentin Aebi <valentin.aebi@epfl.ch>
2023-08-02 22:34:02 +0200

    Add TODO

--- src/main/scala/lang/Signatures.scala
+++ src/main/scala/lang/Signatures.scala
@@ -8,4 +8,5 @@ import scala.collection.mutable
 
 final case class FunctionSignature(name: FunOrVarId, argTypes: List[Type], retType: Type)
 
+// TODO optional const/mut (?) for fields
 final case class StructSignature(name: TypeIdentifier, fields: mutable.LinkedHashMap[FunOrVarId, Type])
