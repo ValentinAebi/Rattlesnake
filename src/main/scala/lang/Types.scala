41a5e78c685b3ac5c4061f534ed4f1224b1f9717
Valentin Aebi <valentin.aebi@epfl.ch>
2024-12-02 19:13:07 +0100

    Display capture descriptor of array type right after `arr`

--- src/main/scala/lang/Types.scala
+++ src/main/scala/lang/Types.scala
@@ -7,17 +7,17 @@ import scala.annotation.targetName
 
 
 object Types {
-  
+
   sealed trait Type {
     def shape: TypeShape
     def captureDescriptor: CaptureDescriptor
   }
-  
+
   final case class CapturingType(shape: TypeShape, captureDescriptor: CaptureDescriptor) extends Type {
     override def toString: String =
       if captureDescriptor.isEmpty then shape.toString
       else if captureDescriptor.isRoot then s"$shape^"
-      else s"$shape^$captureDescriptor"
+      else shape.toStringCapturing(captureDescriptor)
   }
 
   sealed trait TypeShape extends Type {
@@ -26,10 +26,12 @@ object Types {
     @targetName("capturing") infix def ^(cd: CaptureDescriptor): CapturingType = CapturingType(this, cd)
     @targetName("maybeCapturing") infix def ^(cdOpt: Option[CaptureDescriptor]): Type =
       cdOpt.map(CapturingType(this, _)).getOrElse(this)
+    def toStringCapturing(captureDescriptor: CaptureDescriptor): String =
+      s"$toString^$captureDescriptor"
   }
-  
+
   sealed trait CastTargetTypeShape extends TypeShape
-  
+
   enum PrimitiveTypeShape(val str: String) extends CastTargetTypeShape {
     case IntType extends PrimitiveTypeShape("Int")
     case DoubleType extends PrimitiveTypeShape("Double")
@@ -43,11 +45,11 @@ object Types {
 
     override def toString: String = str
   }
-  
+
   def primTypeFor(name: TypeIdentifier): Option[PrimitiveTypeShape] = {
     PrimitiveTypeShape.values.find(_.str == name.stringId)
   }
-  
+
   final case class NamedTypeShape(typeName: TypeIdentifier) extends CastTargetTypeShape {
     override def toString: String = typeName.stringId
   }
@@ -59,10 +61,13 @@ object Types {
    */
   final case class ArrayTypeShape(elemType: Type, modifiable: Boolean) extends TypeShape {
     override def toString: String = {
-      (if modifiable then (Keyword.Mut.str ++ " ") else "") ++ s"arr $elemType"
+      (if modifiable then (Keyword.Mut.str ++ " ") else "") ++ s"${Keyword.Arr.str} $elemType"
     }
+
+    override def toStringCapturing(cd: CaptureDescriptor): String =
+      (if modifiable then (Keyword.Mut.str ++ " ") else "") ++ s"${Keyword.Arr.str}^$cd $elemType"
   }
-  
+
   final case class UnionTypeShape(unitedTypes: Set[TypeShape]) extends TypeShape {
     override def toString: String = unitedTypes.toSeq.sortBy(_.toString).mkString(" | ")
   }
@@ -73,5 +78,5 @@ object Types {
   case object UndefinedTypeShape extends TypeShape {
     override def toString: String = "[undefined type]"
   }
-  
+
 }
