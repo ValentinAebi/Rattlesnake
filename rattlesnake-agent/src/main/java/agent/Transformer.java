cfda9c3ced927d0bd73898d25aee8a20eb31581f
Valentin Aebi <valentin.aebi@epfl.ch>
2024-12-19 17:11:56 +0100

    Log stacktrace and crash program when an error occurs during instrumentation

--- rattlesnake-agent/src/main/java/agent/Transformer.java
+++ rattlesnake-agent/src/main/java/agent/Transformer.java
@@ -3,7 +3,13 @@ package agent;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.instrument.ClassFileTransformer;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.ProtectionDomain;
 
 public final class Transformer implements ClassFileTransformer {
@@ -18,14 +24,30 @@ public final class Transformer implements ClassFileTransformer {
     ) {
         var isProgramClass = !className.contains("/") && !className.contains("$") && !className.equals("FileSystem");
         if (isProgramClass) {
-            System.err.println("[DEBUG] Instrumenting " + className);   // TODO remove (debug)
-            var reader = new ClassReader(classfileBuffer);
-            var writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-            var transformer = new ClassTransformer(writer);
-            reader.accept(transformer, ClassReader.EXPAND_FRAMES);
-            return writer.toByteArray();
+            try {
+                System.err.println("[DEBUG] Instrumenting " + className);   // TODO remove (debug)
+                var reader = new ClassReader(classfileBuffer);
+                var writer = new ClassWriter(reader, ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+                var transformer = new ClassTransformer(writer);
+                reader.accept(transformer, ClassReader.EXPAND_FRAMES);
+                return writer.toByteArray();
+            } catch (Throwable t) {
+                System.err.println("Instrumentation failed");
+                var logFileName = "instrumentation-failed-" + className + "-" + System.nanoTime() + ".log";
+                var logFilePath = Path.of(".").resolve(logFileName);
+                try (var writer = new FileWriter(logFilePath.toFile())){
+                    t.printStackTrace(new PrintWriter(writer));
+                    System.err.println("Stacktrace of the error can be found in " + logFileName);
+                } catch (IOException e) {
+                    System.err.println("Log file could not be written. Stacktrace:");
+                    t.printStackTrace(System.err);
+                    throw new RuntimeException(e);
+                }
+                System.exit(-1);
+                return null;
+            }
         } else {
-            return classfileBuffer;
+            return null;
         }
     }
 
